59e2f79c55798d4751f3517fefb3cc08ec8bf2fd
oursalman <salman@khroc.com>
2023-11-13 23:59:16 +0600

    Create Project

--- app/src/main/java/com/khroc/khroc/View/Activity/MainActivity.kt
+++ app/src/main/java/com/khroc/khroc/View/Activity/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.khroc.khroc
+package com.khroc.khroc.View.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.khroc.khroc.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
---
+++ app/src/main/java/com/khroc/khroc/View/Fragments/SplashScreenFragment.kt
@@ -0,0 +1,29 @@
+package com.khroc.khroc.View.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.khroc.khroc.R
+
+
+
+class SplashScreenFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+
+
+}
